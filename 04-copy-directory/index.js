869e8605d8bf7c51e9b6104d79b566b5fe56c929
iagasi <nameagasi@gmail.com>
2022-11-04 20:16:50 +0400

    feature:04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const path = require("path")
+const fs = require("fs")
+const pathFolder = path.join(__dirname, "files")
+const destinationPath = path.join(__dirname, "files-copy")
+copy()
+function copy() {
 
+
+    fs.mkdir(destinationPath, { recursive: true }, (err) => {
+
+    });
+    fs.readdir(destinationPath, (err, files) => {
+        files.forEach(file => {
+            fs.unlink(path.join(__dirname, "files-copy", file), (err) => { })
+        })
+    })
+
+
+    fs.readdir(pathFolder, (err, files) => {
+        if (err) { console.log(err); }
+        files.forEach(file => {
+            fs.copyFile(path.join(__dirname, "files", file), path.join(__dirname, "files-copy", file), (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            })
+
+
+        })
+    })
+}
