034fb6991d079748ef3ec4dd33b8248aa6cd2e35
iagasi <nameagasi@gmail.com>
2022-11-04 19:22:29 +0400

    refactor:01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,55 @@
-const FileHandler = require("../fileHandler");
-const path = require("path")
+
+const fs = require("fs")
+const path = require("path");
+const { stderr } = require("process");
 const URL = path.resolve(__dirname, "text.txt")
+
+class FileHandler {
+    check(fileUrl, cb) {
+        fs.access(fileUrl, (e) => {
+            if (e) { cb(e); return }
+            else {
+                cb()
+            }
+        })
+    }
+
+
+    read(fileUrl, cb) {
+
+        this.check(fileUrl, () => {
+            const reader = fs.createReadStream(fileUrl, { encoding: "utf-8" })
+            reader.on("data", (data) => { cb(data) })
+        })
+    }
+
+
+    write(fileUrl, data) {
+        this.check(fileUrl, (error) => {
+            if (error) {
+                fs.open(fileUrl, "w", (err) => {
+                    if (err) { console.log(err); return }
+                    else {
+                        const writable = fs.createWriteStream(fileUrl, { flags: "a", encoding: "utf-8" })
+                        writable.write(data)
+
+                    }
+                })
+            }
+            else {
+                const writable = fs.createWriteStream(fileUrl, { flags: "a", encoding: "utf-8" })
+                writable.write(data)
+
+            }
+        })
+    }
+}
+
+
+
+
+
 const f = new FileHandler()
 
-f.read(URL, (data) => { console.log(data);})
-// f.write("")
+f.read(URL, (chunk) => { stderr.write(chunk) })
+
