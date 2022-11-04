1db7b0c5ebe8e13ec18e7aa57d8e532a5e80409e
iagasi <nameagasi@gmail.com>
2022-11-04 19:25:54 +0400

    refactor:02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,12 +1,50 @@
-const FileHandler = require("../fileHandler");
 const path = require("path")
 const readline = require('readline');
 const URL = path.resolve(__dirname, "02-write-file.txt")
-const f = new FileHandler()
+const fs=require("fs")
+
+class FileHandler {
+    check(fileUrl, cb) {
+        fs.access(fileUrl, (e) => {
+            if (e) { cb(e); return }
+            else {
+                cb()
+            }
+        })
+    }
 
 
+    read(fileUrl, cb) {
+
+        this.check(fileUrl, () => {
+            const reader = fs.createReadStream(fileUrl, { encoding: "utf-8" })
+            reader.on("data", (data) => { cb(data) })
+        })
+    }
 
 
+    write(fileUrl, data) {
+        this.check(fileUrl, (error) => {
+            if (error) {
+                fs.open(fileUrl, "w", (err) => {
+                    if (err) { console.log(err); return }
+                    else {
+                        const writable = fs.createWriteStream(fileUrl, { flags: "a", encoding: "utf-8" })
+                        writable.write(data)
+
+                    }
+                })
+            }
+            else {
+                const writable = fs.createWriteStream(fileUrl, { flags: "a", encoding: "utf-8" })
+                writable.write(data)
+
+            }
+        })
+    }
+}
+
+const f=new FileHandler()
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -14,8 +52,8 @@ const rl = readline.createInterface({
 
 rl.write('Writing Mode:\n ');
 rl.on("line", (str) => {
-    if (str == "exit") {rl.close().process.close(0) }
-    f.write(URL,str)
+    if (str == "exit") { rl.close().process.close(0) }
+    f.write(URL, str)
 })
 rl.on('close', function () {
     console.log('\nBYE BYE !!!');
